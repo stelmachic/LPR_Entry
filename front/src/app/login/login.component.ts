a7dcf6129ec4858c784b1a85f6103028a40c7585
carloseadac <carloseadac@gmail.com>
2022-11-08 14:50:05 -0300

    alguem altera a tela de login ai :(

--- front/src/app/login/login.component.ts
+++ front/src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import axios from 'axios';
+import { Route, Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
+  login(){
+    let edv = document.getElementById("EDV") as HTMLInputElement;
+    let password = document.getElementById("password") as HTMLInputElement;
 
+    var data = JSON.stringify({
+      "edv": edv?.value,
+      "password": password?.value
+    });
+
+    let self = this;
+    var config = {
+      method: 'post',
+      url: 'http://localhost:5051/user/login',
+      headers: { 
+        'Content-Type': 'application/json'
+      },
+      data : data
+    };
+
+    axios(config)
+    .then(function (response:any) {
+      localStorage.setItem('authToken',response.data);
+      self.router.navigate(['home']);
+    })
+    .catch(function (error:any) {
+      console.log(error);
+    });
+
+  }
 }
